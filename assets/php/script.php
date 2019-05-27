9de3d76608dabac0e8a4bb3168616f769887c3e5
sori883 <service@dlme.biz>
2019-05-28 05:54:05 +0900

    waiwai

--- assets/php/script.php
+++ assets/php/script.php
@@ -1,45 +1,48 @@
 <?php
+////////////////////////////////////////////////////////////
+//init
+////////////////////////////////////////////////////////////
 $encode = "UTF-8"
 $site_top = "https://sori883.com/";
 $to = "hello@sori883.com";
-$Referer_check_domain = "sori883.com";
+$subject = "ポートフォリオのお問い合わせ";
+
 $email = $_POST['Email'];
 $message = $_POST['Message'];
-$subject = "ポートフォリオのお問い合わせ";
 
 //タイムゾーン
 date_default_timezone_set('Asia/Tokyo');
 
 
 
-//メールチェック
-if(empty($email){
-    if(!checkMail($val)){
-    $errm .= "<p class=\"error_messe\">【".$key."】はメールアドレスの形式が正しくありません。</p>\n";
-    $empty_flag = 1;
-    }
-}
+////////////////////////////////////////////////////////////
+//実行
+////////////////////////////////////////////////////////////
 
-//メール送信
-mail($to,$subject,$adminBody,$header);
-
-
-
-
-
-function checkMail($str){
-	$mailaddress_array = explode('@',$str);
-	if(preg_match("/^[\.!#%&\-_0-9a-zA-Z\?\/\+]+\@[!#%&\-_0-9a-z]+(\.[!#%&\-_0-9a-z]+)+$/", "$str") && count($mailaddress_array) ==2){
+//メール形式確認
+function checkMail($mail){
+	$mailaddress_array = explode('@',$mail);
+	if(preg_match("/^[\.!#%&\-_0-9a-zA-Z\?\/\+]+\@[!#%&\-_0-9a-z]+(\.[!#%&\-_0-9a-z]+)+$/", "$mail") && count($mailaddress_array) ==2){
 		return true;
 	}else{
 		return false;
 	}
 }
 
-//htmlタグの無効化
-function h($string) {
+//htmlタグとかをプレーンテキストに変換
+function htmlEntity($string) {
 	global $encode;
 	return htmlspecialchars($string, ENT_QUOTES,$encode);
 }
+
+
+////////////////////////////////////////////////////////////
+//関数
+////////////////////////////////////////////////////////////
+
+
+
+
+
 
 ?>
