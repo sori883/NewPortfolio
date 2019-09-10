8107eb3b218ddbb2a70bd1bba450da50e4d9282d
sori883 <service@dlme.biz>
2019-09-10 22:55:27 +0900

    サイドバーが開かないの修正

--- src/assets/js_orijin/script.js
+++ src/assets/js_orijin/script.js
@@ -5,17 +5,18 @@ $(".first-down-icon").click(function(){
   });
 });
 
+// ページ内リンクのスクロール
+$('a[href^="#"]').click(function(){
+  var speed = 500;
+  var href= $(this).attr("href");
+  var target = $(href == "#" || href == "" ? 'html' : href);
+  var position = target.offset().top;
+  $("html, body").animate({scrollTop:position}, speed, "swing");
+  return false;
+});
+
 // スライドバー
 $(document).ready(function(){
   $('.sidenav').sidenav();
 });
 
-// ページ内リンクのスクロール
-$('a[href^="#"]').click(function() {
-  var speed = 400;
-  var href= $(this).attr("href");
-  var target = $(href == "#" || href == "" ? 'html' : href);
-  var position = target.offset().top;
-  $('body,html').animate({scrollTop:position}, speed, 'swing');
-  return false;
-});
